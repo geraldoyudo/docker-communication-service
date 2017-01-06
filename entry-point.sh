97a600d8f85dca4c4485947fe5447a941183ff96
Oyudo Gerald <gerald.oyudo@gmail.com>
2017-01-06 12:55:45 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $fileName
 echo $log
-java -jar $fileName --spring.config.location=/conf/application.yml >> $log
+java -jar $fileName --spring.config.location=/conf/application.yml >> $logFolder
 echo "Application started successfully"
